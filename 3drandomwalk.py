b148ffe86a3480f31ec1e37e9f2e62f62189b3d5
Evan Batteas <64985672+EvanBatteas@users.noreply.github.com>
2022-10-20 17:17:18 -0500

    Update 3drandomwalk.py
    added red dot at origin and abilty to plot on keyboard interrupt

--- 3drandomwalk.py
+++ 3drandomwalk.py
@@ -25,21 +25,31 @@ print(origin)
 pathx.append(origin[0])
 pathy.append(origin[1])
 pathz.append(origin[2])  
-atorigin = True  
-while(atorigin):
+atorigin = True
+multiplier = 1
+x=0  
+try:
+ while(atorigin):
+  distance = (origin[0] **2 + origin[1] **2 + origin[2]**2)**.5 - 100 * multiplier #flow acceleration for 3d
+  if(distance > 100):
+    multiplier = 2 +x
+    x+=1
+  elif(distance < 100):
+    multiplier = 1 
+    x = 0
   chosen = random.randint(1,6)
   if(chosen==1):
-    origin[0] +=1
+    origin[0] +=1 *multiplier
   elif(chosen==2):
-    origin[1] +=1
+    origin[1] +=1 *multiplier
   elif(chosen==3):
-    origin[0] -= 1
+    origin[0] -= 1 *multiplier
   elif(chosen==4):
-    origin[1] -=1
+    origin[1] -=1 *multiplier
   elif(chosen==5):
-   origin[2]+=1
+   origin[2]+=1 *multiplier
   elif(chosen==6):
-    origin[2]-=1   
+    origin[2]-=1 *multiplier
   else:
     print("Out of Range")
   pathx.append(origin[0])
@@ -48,8 +58,14 @@ while(atorigin):
   #plt.plot(pathx,pathy) 
   if(origin[0] == 0 and origin[1] == 0 and origin[2] == 0):
     atorigin = False 
-  #print(origin) 
+except KeyboardInterrupt:
+    #if it takes to long i can just interrupt it, and it will still plot, just not returning to origing 
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')   
+    ax.plot3D(pathx, pathy, pathz, 'blue')
+    ax.scatter(0, 0, 0, c='red', marker='.', s=10)
 fig = plt.figure()
 ax = plt.axes(projection='3d')   
 ax.plot3D(pathx, pathy, pathz, 'blue')
+ax.scatter(0, 0, 0, c='red', marker='.', s=10)
 #ax.plot3D(0,0,0,'.','red')
